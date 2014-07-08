bd5ee4c8fa9972acc6660bea5aa4ede2370fa9a4
Kevin Ballard <kevin@sb.org>
2014-07-07 23:29:18 -0700

    Convert comments to doc comments

--- tsao.swift
+++ tsao.swift
@@ -2,17 +2,17 @@
 
 import Foundation
 
-// AssocKey represents an associated objects key, with a given value type.
-//
-// Create new keys with newAssocKey(). Keys should be toplevel let-bindings
-// and should never deinit. If a key deinits, an assertion is thrown.
-// This is to preserve the type-safety of the associated values.
-//
-// Keys with a class value type may use the assign policy, and keys with a
-// value type that conforms to NSCopying may use the copy policy. Value types
-// must always use the default retain policy.
-//
-// @see newAssocKey
+/// AssocKey represents an associated objects key, with a given value type.
+///
+/// Create new keys with newAssocKey(). Keys should be toplevel let-bindings
+/// and should never deinit. If a key deinits, an assertion is thrown.
+/// This is to preserve the type-safety of the associated values.
+///
+/// Keys with a class value type may use the assign policy, and keys with a
+/// value type that conforms to NSCopying may use the copy policy. Value types
+/// must always use the default retain policy.
+///
+/// @see newAssocKey
 class AssocKey<ValueType> {
     let _policy: objc_AssociationPolicy
 
@@ -25,30 +25,31 @@ class AssocKey<ValueType> {
     }
 }
 
-// Create a new AssocKey for any value.
-//
-// This uses the retain policy.
+/// Create a new AssocKey for any value.
+///
+/// This uses the retain policy.
 func newAssocKey<ValueType>(atomic: Bool = false) -> AssocKey<ValueType> {
     let policy = atomic ? OBJC_ASSOCIATION_RETAIN : OBJC_ASSOCIATION_RETAIN_NONATOMIC
     return AssocKey(_private: objc_AssociationPolicy(policy))
 }
 
-// Create a new AssocKey for any class value with the assign policy.
-//
-// This looks a bit weird, but it can be invoked as <tt>newAssocKey(assign: ())</tt>.
+/// Create a new AssocKey for any class value with the assign policy.
+///
+/// This looks a bit weird, but it can be invoked as
+/// <tt>newAssocKey(assign: ())</tt>.
 func newAssocKey<ValueType: AnyObject>(#assign: ()) -> AssocKey<ValueType> {
     return AssocKey(_private: objc_AssociationPolicy(OBJC_ASSOCIATION_ASSIGN))
 }
 
-// Create a new AssocKey for any NSCopying value with the copy policy.
+/// Create a new AssocKey for any NSCopying value with the copy policy.
 func newAssocKey<ValueType: NSCopying>(copyAtomic atomic: Bool) -> AssocKey<ValueType> {
     let policy = atomic ? OBJC_ASSOCIATION_COPY : OBJC_ASSOCIATION_COPY_NONATOMIC
     return AssocKey(_private: objc_AssociationPolicy(policy))
 }
 
-// The data type that mediates access to associated objects.
-//
-// Use the associatedObjects() function to create an instance of this type.
+/// The data type that mediates access to associated objects.
+///
+/// Use the associatedObjects() function to create an instance of this type.
 struct AssociatedObjectView {
     var _object: AnyObject
 
@@ -56,10 +57,10 @@ struct AssociatedObjectView {
         _object = object
     }
 
-    // Get an associated object for a given key.
-    //
-    // Ideally this would be a subscript operator but those don't support
-    // generics.
+    /// Get an associated object for a given key.
+    ///
+    /// Ideally this would be a subscript operator but those don't support
+    /// generics.
     func get<ValueType>(key: AssocKey<ValueType>) -> ValueType? {
         return _get(key) {
             // skip the runtime type test, we know it's the right type
@@ -86,10 +87,10 @@ struct AssociatedObjectView {
         }
     }
 
-    // Set an associated object for a given key.
-    //
-    // Ideally this would be a mutating subscript operator but those don't
-    // support generics.
+    /// Set an associated object for a given key.
+    ///
+    /// Ideally this would be a mutating subscript operator but those don't
+    /// support generics.
     func set<ValueType>(key: AssocKey<ValueType>, value: ValueType?) {
         if let v = value {
             _set(key, _AssocValueBox(v))
@@ -125,7 +126,7 @@ class _AssocValueBox<ValueType> {
     }
 }
 
-// Retrieve the associated object mapping for a given object.
+/// Retrieve the associated object mapping for a given object.
 func associatedObjects(object: AnyObject) -> AssociatedObjectView {
     return AssociatedObjectView(_private: object)
 }

48d60bd10fd8b3c0af7620e4497c46629a548c0b
Kevin Ballard <kevin@sb.org>
2015-02-09 23:49:27 -0800

    Update Makefile to build an actual module
    Build a module + library in order to run the test, instead of compiling
    the test and library source together.

--- Makefile
+++ Makefile
@@ -1,11 +1,22 @@
-.PHONY: test
+.PHONY: test clean
+.DEFAULT_GOAL := test
 
 SWIFTC=xcrun swiftc -sdk $$(xcrun -show-sdk-path -sdk macosx)
+SOURCE_FILES := tsao.swift
 
-.INTERMEDIATE: exec-test
+target:
+	mkdir target
 
-test: exec-test
-	./exec-test
+target/libTSAO.dylib: $(SOURCE_FILES) | target
+	$(SWIFTC) -emit-library -emit-module -module-name TSAO -module-link-name TSAO -O -o target/libTSAO.dylib $(SOURCE_FILES)
 
-exec-test: test.swift tsao.swift
-	$(SWIFTC) -o exec-test tsao.swift test.swift
+target/test: target/libTSAO.dylib
+	$(SWIFTC) -I target/ -L target/ -o target/test test.swift
+
+.INTERMEDIATE: target/test
+
+test: target/test
+	./target/test
+
+clean:
+	rm -rf target/
--- test.swift
+++ test.swift
@@ -1,4 +1,5 @@
 import Foundation
+import TSAO
 
 let intKey: AssocKey<Int> = newAssocKey()
 let strKey: AssocKey<NSString> = newAssocKey(copyAtomic: false)
@@ -22,38 +23,28 @@ func setup() -> Foo {
     return f
 }
 
-// This is hacky, but I don't see any other way to do this right now, short of
-// renaming the file to `main.swift`.
-@asmname("main")
-func main(argc: CInt, argv: UnsafeMutablePointer<UnsafeMutablePointer<Int8>>) -> CInt {
-    top_level_code()
-    return 0
-}
+let f = setup()
 
-func top_level_code() {
-    let f = setup()
+println("\nFetching associated objects...")
+// note that each one of these method calls is returning the actual type
+// associated with the key, instead of returning Any or AnyObject
+println("intKey: \(associatedObjects(f).get(intKey))") // returns Int?
+println("strKey: \(associatedObjects(f).get(strKey))") // returns NSString?
+println("aryKey: \(associatedObjects(f).get(aryKey))") // returns String[]?
 
-    println("\nFetching associated objects...")
-    // note that each one of these method calls is returning the actual type
-    // associated with the key, instead of returning Any or AnyObject
-    println("intKey: \(associatedObjects(f).get(intKey))") // returns Int?
-    println("strKey: \(associatedObjects(f).get(strKey))") // returns NSString?
-    println("aryKey: \(associatedObjects(f).get(aryKey))") // returns String[]?
+println("\nFetching unset associated object...")
+println("intKey2: \(associatedObjects(f).get(intKey2))") // returns Int?
 
-    println("\nFetching unset associated object...")
-    println("intKey2: \(associatedObjects(f).get(intKey2))") // returns Int?
+println("\nSetting a copy associated value...")
+let s = NSMutableString(string: "mutable string")
+associatedObjects(f).set(strKey, value: s) // takes NSString?
+println("strKey: \(s)")
+println("\nMutating string...")
+s.appendString(" with changes")
+println("mutated: \(s)")
+println("\nFetching copy associated value...")
+println("strKey: \(associatedObjects(f).get(strKey))") // returns NSString?
 
-    println("\nSetting a copy associated value...")
-    let s = NSMutableString(string: "mutable string")
-    associatedObjects(f).set(strKey, value: s) // takes NSString?
-    println("strKey: \(s)")
-    println("\nMutating string...")
-    s.appendString(" with changes")
-    println("mutated: \(s)")
-    println("\nFetching copy associated value...")
-    println("strKey: \(associatedObjects(f).get(strKey))") // returns NSString?
-
-    println("\nClearing associated value...")
-    associatedObjects(f).set(aryKey, value: nil)           // takes String[]?
-    println("aryKey: \(associatedObjects(f).get(aryKey))") // returns String[]?
-}
+println("\nClearing associated value...")
+associatedObjects(f).set(aryKey, value: nil)           // takes String[]?
+println("aryKey: \(associatedObjects(f).get(aryKey))") // returns String[]?
